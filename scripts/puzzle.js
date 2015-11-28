c0ba3db023c89be989775f14fab75c64a4ff9c21
huhk-sysu <438557194@qq.com>
2015-11-28 22:06:15 +0800

    v3

--- scripts/puzzle.js
+++ scripts/puzzle.js
@@ -15,7 +15,8 @@ var globalVariable = {
     timer: -1,
     solution: [],
     clickValid: true,
-    pictureUrl: ["images/panda.jpg", "images/meinv.jpg", "images/wangqing.jpg"]
+    pictureUrl: ["images/panda.jpg", "images/meinv.jpg", "images/wangqing.jpg"],
+    boxes: null
 };
 
 function createBoxes() {
@@ -50,18 +51,22 @@ function canMove(myId) {
             return true;
         }
     }
-    // console.log("can't move!");
     return false;
 }
 
 function restartValid(valid) {
     "use strict";
-    var restart = $("#restart"), resign = $("#resign"), pictureSelector = $("#pictureSelector");
+    var restart = $("#restart"), pictureSelector = $("#pictureSelector");
     restart.attr("disabled", !valid);
     restart.toggleClass("disabled");
-    resign.attr("disabled", valid);
-    resign.toggleClass("disabled");
     pictureSelector.attr("disabled", !valid);
+}
+
+function resignValid(valid) {
+    "use strict";
+    var resign = $("#resign");
+    resign.attr("disabled", !valid);
+    resign.toggleClass("disabled");
 }
 
 function gameOver(boxes) {
@@ -75,41 +80,38 @@ function gameOver(boxes) {
     return true;
 }
 
+function changeData(myId) {
+    "use strict";
+    var target = $("#box15"), myself = $("#box" + myId), temp;
+    target.removeClass("left" + globalVariable.freeY + " " + "top" + globalVariable.freeX);
+    target.addClass("left" + globalVariable.positionY[myId] + " " + "top" + globalVariable.positionX[myId]);
+    myself.removeClass("left" + globalVariable.positionY[myId] + " " + "top" + globalVariable.positionX[myId]);
+    myself.addClass("left" + globalVariable.freeY + " " + "top" + globalVariable.freeX);
+    temp = globalVariable.freeX;
+    globalVariable.freeX = globalVariable.positionX[myId];
+    globalVariable.positionX[myId] = temp;
+    temp = globalVariable.freeY;
+    globalVariable.freeY = globalVariable.positionY[myId];
+    globalVariable.positionY[myId] = temp;
+}
+
+function showGameOver() {
+    "use strict";
+    alert("你在" + globalVariable.timeCounter.toFixed(1) + "秒内走了" + globalVariable.moveCounter + "步，最终获得了胜利！");
+    globalVariable.gameStatus = globalVariable.STOP;
+    clearInterval(globalVariable.timer);
+    restartValid(true);
+    resignValid(false);
+}
+
 function boxesClick(myId) {
     "use strict";
     return function () {
-        if (globalVariable.gameStatus === globalVariable.STOP || !globalVariable.clickValid) {
-            return;
-        }
-        var ok = canMove(myId), target, myself, temp, boxes = $(".box");
-        if (ok) {
-            // change position
-            // 先移除后添加
-            target = $("#box15");
-            myself = $("#box" + myId);
-            target.removeClass("left" + globalVariable.freeY);
-            target.removeClass("top" + globalVariable.freeX);
-            target.addClass("left" + globalVariable.positionY[myId]);
-            target.addClass("top" + globalVariable.positionX[myId]);
-            myself.removeClass("left" + globalVariable.positionY[myId]);
-            myself.removeClass("top" + globalVariable.positionX[myId]);
-            myself.addClass("left" + globalVariable.freeY);
-            myself.addClass("top" + globalVariable.freeX);
-
-            // change data
-            temp = globalVariable.freeX;
-            globalVariable.freeX = globalVariable.positionX[myId];
-            globalVariable.positionX[myId] = temp;
-
-            temp = globalVariable.freeY;
-            globalVariable.freeY = globalVariable.positionY[myId];
-            globalVariable.positionY[myId] = temp;
-
+        if (globalVariable.gameStatus !== globalVariable.STOP && globalVariable.clickValid && canMove(myId)) {
+            changeData(myId);
             if (globalVariable.gameStatus === globalVariable.PREPARING) {
                 globalVariable.solution.push(myId);
-            }
-
-            if (globalVariable.gameStatus === globalVariable.RUNNING) {
+            } else if (globalVariable.gameStatus === globalVariable.RUNNING) {
                 ++globalVariable.moveCounter;
                 $("#moveCounter").text("步数： " + globalVariable.moveCounter);
                 if (globalVariable.solution[globalVariable.solution.length - 1] === myId) {
@@ -117,11 +119,8 @@ function boxesClick(myId) {
                 } else {
                     globalVariable.solution.push(myId);
                 }
-                if (gameOver(boxes)) {
-                    alert("你在" + globalVariable.timeCounter.toFixed(1) + "秒内走了" + globalVariable.moveCounter + "步，最终获得了胜利！");
-                    globalVariable.gameStatus = globalVariable.STOP;
-                    clearInterval(globalVariable.timer);
-                    restartValid(true);
+                if (gameOver(globalVariable.boxes)) {
+                    showGameOver();
                 }
             }
         }
@@ -151,11 +150,11 @@ function demo() {
 
 function randomMove() {
     "use strict";
-    var boxes = $(".box"), counter = 0, rand, times = _.random(40, 60);  // move 40~60 times
+    var counter = 0, rand, times = _.random(40, 60);  // move 40~60 times
     while (counter < times) {
         rand = _.random(0, 14);
         if (rand !== globalVariable.solution[globalVariable.solution.length - 1] && canMove(rand)) {  // 不重复动2次同一个
-            boxes.eq(rand).click();
+            globalVariable.boxes.eq(rand).click();
             ++counter;
         }
     }
@@ -172,6 +171,7 @@ function restartClick() {
     globalVariable.timeCounter = 0;
     $("#time").text("时间： " + globalVariable.timeCounter.toFixed(1) + "秒");
     restartValid(false);
+    resignValid(true);
     globalVariable.timer = setInterval(update, 100);
 }
 
@@ -186,48 +186,42 @@ function resignClick() {
     globalVariable.timer = setInterval(demo, 100);
 }
 
+function modeClick() {
+    "use strict";
+    var boxes = $(".box");
+    _.times(15, function (i) {
+        boxes.eq(i).toggleClass("boxNum");
+    });
+}
 
+function selectorChange() {
+    "use strict";
+    var index = $("#pictureSelector").get()[0].selectedIndex;
+    $("#panda").attr("src", globalVariable.pictureUrl[index]);
+    _.times(15, function (i) {
+        globalVariable.boxes.eq(i).removeClass("boxPic1 boxPic2 boxPic3");
+        globalVariable.boxes.eq(i).addClass("boxPic" + (index + 1));
+    });
+}
+
+function setButtons() {
+    "use strict";
+    $("#restart").click(restartClick);
+    $("#resign").click(resignClick);
+    $("#mode").change(modeClick);
+    $("#pictureSelector").change(selectorChange);
+}
 
 function prepare() {
     "use strict";
-    var boxes = $(".box"), restart = $("#restart"), resign = $("#resign"), mode = $("#mode"), picture = $("#panda"),
-        pictureSelector = $("#pictureSelector"), i;
+    globalVariable.boxes = $(".box");
     _.times(15, function (i) {
-        boxes.eq(i).mousedown(demoMouseDown);
-        boxes.eq(i).click(boxesClick(i));
+        globalVariable.boxes.eq(i).mousedown(demoMouseDown);
+        globalVariable.boxes.eq(i).click(boxesClick(i));
     });
-    boxes.eq(15).hide();
-    restart.click(restartClick);
-    resign.click(resignClick);
-    mode.change(function () {
-        _.times(15, function (i) {
-            boxes.eq(i).toggleClass("boxNum");
-        });
-    });
-
-    pictureSelector.change(function () {
-        var index = this.selectedIndex;
-        picture.attr("src", globalVariable.pictureUrl[index]);
-        if (index === 0) {
-            for (i = 0; i < boxes.length - 1; ++i) {
-                boxes.eq(i).removeClass("boxPic2");
-                boxes.eq(i).removeClass("boxPic3");
-            }
-        } else if (index === 1) {
-            for (i = 0; i < boxes.length - 1; ++i) {
-                boxes.eq(i).addClass("boxPic2");
-                boxes.eq(i).removeClass("boxPic3");
-            }
-        } else {
-            for (i = 0; i < boxes.length - 1; ++i) {
-                boxes.eq(i).removeClass("boxPic2");
-                boxes.eq(i).addClass("boxPic3");
-            }
-        }
-    });
-
-    resign.attr("disabled", true);
-    resign.addClass("disabled");
+    globalVariable.boxes.eq(15).hide();
+    setButtons();
+    resignValid(false);
     globalVariable.gameStatus = globalVariable.STOP;
 }
 
